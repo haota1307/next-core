445723a9af9d330f98b71112cb766a65bff4afb9
haota1307 <trananhhao1307@gmail.com>
2025-08-16 14:37:19 +0700

    Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <ModeToggle />
+    </main>
+  );
+}
